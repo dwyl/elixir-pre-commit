f3485767f90d8ed5742a0958b36fe850851ff520
finnhodgkin <finnhodgkin@gmail.com>
2017-10-12 10:59:28 +0100

    refactor bash script #6

--- lib/elixir_pre_commit.ex
+++ lib/elixir_pre_commit.ex
@@ -1,6 +1,11 @@
 defmodule ElixirPreCommit do
   @moduledoc """
-  config :elixir_pre_commit, commands: ["test", "coveralls", "credo"]
+  This module copies our pre-commit setup to `/.git/hooks/`.
+
+  To choose which of your mix tasks to run as a pre-commit add the following
+  to your config:
+  
+  `config :elixir_pre_commit, commands: ["test", "coveralls", "credo"]`
   """
   copy = Mix.Project.deps_path() |> Path.join("../priv/pre-commit")
   to = Mix.Project.deps_path() |> Path.join("../.git/hooks/pre-commit")
